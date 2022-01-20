5f0ea23dbb9ebd59b9e382e6258e158486370234
Dima Oreh <d.oreh@mail.ru>
2022-01-20 18:30:17 +0300

    feat: fix like

--- script/index.js
+++ script/index.js
@@ -1,5 +1,6 @@
+// POPUP PROFILE
 const popupOpen = document.querySelector('.profile__edit');
-const popup = document.querySelector('.popup');
+const popup = document.querySelector('.popup_profile');
 const popupClose = document.querySelector('.popup__close');
 let nameInput = document.querySelector('.popup__user_name');
 let jobInput = document.querySelector('.popup__user_job');
@@ -8,6 +9,50 @@ let profileName = document.querySelector('.profile__name');
 let profileJob = document.querySelector('.profile__job');   
 let formElement = document.querySelector('.popup__form');
 
+//POPUP CARDS
+const popupOpenCards = document.querySelector('.profile__button');
+const popupCards = document.querySelector('.popup_cards');
+const popupCloseCards = document.querySelector('.popup__close_cards');
+
+const initialCards = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+  ];
+
+
+
+
+// LIKE 
+let likeButton = document.querySelector('.cards__like');
+
+likeButton.addEventListener('click', function() {
+  likeButton.classList.toggle('cards__like_active');
+})
+
+
+// PROFILE 
 
 function openPopup() {
     nameInput.value = profileName.textContent;
@@ -23,8 +68,7 @@ function closePopup() {
 popupOpen.addEventListener('click', openPopup);
 popupClose.addEventListener('click', closePopup);
 
-// Находим форму в DOM// Воспользуйтесь методом querySelector()
-// Находим поля формы в DOM
+
 
 
 // Обработчик «отправки» формы, хотя пока
@@ -45,8 +89,28 @@ function formSubmitHandler(evt) {
 
 }
 
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
+
 formElement.addEventListener('submit', formSubmitHandler);
 
+// CARDS FUNCTION 
 
+function popupCardsOpen() {
+    popupCards.classList.add('popup_opened');
+}
+
+function popupCardsClose() {
+    popupCards.classList.remove('popup_opened');
+}
+
+popupOpenCards.addEventListener('click', popupCardsOpen);
+popupCloseCards.addEventListener('click', popupCardsClose);
+
+//ADD CARDS
+
+function addCard(textValue, imageValue) {
+  const cardTemplate = document.querySelector('#cards-template').content;
+  const cardElement = cardTemplate.querySelector('.cards').cloneNode(true);
+
+  cardElement.querySelector('.cards__text').textContent = textValue;
+  cardElement.querySelector('cards__image').src = imageValue;
+}
