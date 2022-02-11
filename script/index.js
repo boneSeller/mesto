d456558f49524719961bb8265cda1aa9045493e5
Dima Oreh <d.oreh@mail.ru>
2022-02-11 11:25:56 +0300

    feat: add error massage

--- script/index.js
+++ script/index.js
@@ -2,12 +2,9 @@
 const popupOpenProfile = document.querySelector('.profile__edit');
 const popupProfile = document.querySelector('.popup_profile');
 const popupCloseProfile = document.querySelector('.popup__close_profile');
-const nameInput = document.querySelector('.popup__user_name');
-const jobInput = document.querySelector('.popup__user_job');
 const popupSaveProfile = document.querySelector('.popup__save_profile');
 const profileName = document.querySelector('.profile__name');
 const profileJob = document.querySelector('.profile__job');   
-const formElement = document.querySelector('.popup__form_profile');
 const popupCard = document.querySelector('.popup_cards');
 const popupImage = document.querySelector('.popup_image');
 const popupOpenCards = document.querySelector('.profile__button');
@@ -16,26 +13,43 @@ const imageText = document.querySelector('.popup__text');
 const imagePopupUrl = document.querySelector('.popup__image'); 
 const textCards = document.querySelector('.cards__text');
 const savePopupCard = document.querySelector('.popup__save_cards');
-const popupText = document.querySelector('.popup__user_title');
-const popupLink = document.querySelector('.popup__user_link');
-const formCards = document.querySelector('.popup__form_cards');
 const popupCloseImage = document.querySelector('.popup__close_image');
 const cardContainer = document.querySelector('.elements');
+const formProfile = document.forms.formProfile;
+const username = formProfile.elements.username;
+const userjob = formProfile.elements.userjob;
+const formCard = document.forms.formCards;
+const usertittle = formCard.elements.usertitle;
+const userlink = formCard.elements.userlink;
 
 // PROFILE 
 const OpenProfilePopup = () => {
-  nameInput.value = profileName.textContent;
-  jobInput.value = profileJob.textContent;
+  username.value = profileName.textContent;
+  userjob.value = profileJob.textContent;
   openPopup(popupProfile)
 }
 
 
 function handleSubmitProfileForm(evt) {
     evt.preventDefault(); 
-    profileName.textContent = nameInput.value;
-    profileJob.textContent = jobInput.value;
+    profileName.textContent = username.value;
+    profileJob.textContent = userjob.value;
+    setSubmitButton(false, popupSaveProfile);
     closePopup(popupProfile);
 }
+
+function setSubmitButton(isFormValid, savebutton) {
+  if (isFormValid) {
+    savebutton.removeAttribute('disabled');
+    savebutton.classList.remove('popup__save_disabled')
+  }
+
+  else { 
+    savebutton.setAttribute('disabled', true);
+    savebutton.classList.add('popup__save_disabled')
+  }
+}
+
 
 
 function createCard(name,link) {
@@ -80,7 +94,6 @@ function createCard(name,link) {
 
 function openPopup(popup) {
   popup.classList.add('popup_opened');
-  
 }
 
 function closePopup(popup) {
@@ -92,22 +105,34 @@ function closePopup(popup) {
 function handleSubmitCardsForm(evt) {
   evt.preventDefault(); 
   const cardInfo = {
-    name: popupText.value,
-    link: popupLink.value
+    name: usertittle.value ,
+    link: userlink.value
   }
+  formCard.reset();
   createNewCard(cardInfo);
+  setSubmitButton(false, savePopupCard);
   closePopup(popupCard);
 }
 
 
 
 
+
 popupOpenProfile.addEventListener('click', OpenProfilePopup);
 popupCloseProfile.addEventListener('click', () => {closePopup(popupProfile)});
-formElement.addEventListener('submit', handleSubmitProfileForm);
+formProfile.addEventListener('submit', handleSubmitProfileForm);
 popupOpenCards.addEventListener('click', () => {openPopup(popupCard)});
 popupCloseCards.addEventListener('click', () => { closePopup(popupCard)});
 popupCloseImage.addEventListener('click', () => { closePopup(popupImage)});
-formCards.addEventListener('submit', handleSubmitCardsForm);
+formCard.addEventListener('submit', handleSubmitCardsForm);
 
 //for git
+formProfile.addEventListener('input', (evt) => {
+  const isValid = username.value.length > 0 && userjob.value.length > 0;
+  setSubmitButton(isValid, popupSaveProfile);
+});
+
+formCard.addEventListener('input', (evt) => {
+  const isValid = usertittle.value.length > 0 && userlink.value.length > 0;
+  setSubmitButton(isValid, savePopupCard);
+});
